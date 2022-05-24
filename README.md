6fb4e2baf0da52eece48d99b715f1415a915f4ee
Fede Herz <fede.herz@gmail.com>
2022-05-24 14:10:15 -0300

    regularize files

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# odoo14-custom-addons
+# Odoo 14 Community Edition - Custom-addons
+
+Odoo modules added to current community installation tu support argentina localization and some others addons.
+
+## Codize Argentina Localization
+
+
--- l10n_ar_financial_reports/wizard/financial_reports.py
+++ l10n_ar_financial_reports/wizard/financial_reports.py
@@ -1,4 +1,7 @@
 from odoo import api, fields, models, _
+import logging
+# import odoo.addons.decimal_precision as dp
+_logger = logging.getLogger(__name__)
 
 class FinancialReports(models.TransientModel):
     _name="ar.financial.reports"
@@ -8,17 +11,33 @@ class FinancialReports(models.TransientModel):
                                     ('ivapurchase', 'Iva Compra')], string="Reporte", default='ivasale')
     date_from = fields.Date (string="Fecha Inicio")
     date_to = fields.Date (string="Fecha Fin")
+    # journal_ids = fields.Many2many('account.journal',
+    #                                'account_report_general_ledger_journal_rel',
+    #                                'account_id', 'journal_id', string='Journals',
+    #                                required=True)
 
     def print_report(self):
         if self.type_report == 'ivasale':
-            invoices_ids = self.env['account.move'].search_read([('move_type','in',['out_invoice','out_refund']),('invoice_date','>=',self.date_from),('invoice_date','<=',self.date_to)])
+            _logger.info('account.move read ivasale')
+            journal_ids =  [journal.id for journal in self.env['account.journal'].search([('company_id', '=', self.env.company.id), ('type', '=', 'sale'), ('l10n_latam_use_documents', '=', True)])]
+            # journals = self.env['account.journal'].search([('company_id', '=', company_id), ('type', '=', 'purchase')])
+            _logger.info('-- journals start --')
+            _logger.info(journal_ids)
+            _logger.info('-- journals end --')
+            # _logger.info('journals: ', journals)
+            # journals_ids = self.env['account.move'].search_read([('move_type','in',['out_invoice','out_refund']),('invoice_date','>=',self.date_from),('invoice_date','<=',self.date_to)])
+            invoices_ids = self.env['account.move'].search_read(
+                [('move_type','in',['out_invoice','out_refund']),('invoice_date','>=',self.date_from),('invoice_date','<=',self.date_to),('journal_id', 'in', journal_ids)])
+            _logger.info('account.move read ivasale OK')
             data = {
                 'from_data' : self.read()[0],
                 'invoices_ids' : invoices_ids
             }
             return self.env.ref('l10n_ar_financial_reports.action_report_iva_sale').sudo().with_context(landscape=True).report_action(self, data=data)
         elif self.type_report == 'ivapurchase':
+            _logger.info('account.move read ivapurchase')
             invoices_ids = self.env['account.move'].search_read([('move_type','in',['in_invoice','in_refund']),('date','>=',self.date_from),('date','<=',self.date_to)])
+            _logger.info('account.move read ivapurchase OK')
             data = {
                 'from_data' : self.read()[0],
                 'invoices_ids' : invoices_ids
